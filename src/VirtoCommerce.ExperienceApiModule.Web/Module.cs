24ee764095c7132bbc60035e6c466e43cbcbad67
Filip Kuksov <vectorfield4@gmail.com>
2020-06-16 15:44:30 +0200

    build: DI

--- src/VirtoCommerce.ExperienceApiModule.Web/Module.cs
+++ src/VirtoCommerce.ExperienceApiModule.Web/Module.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.ExperienceApiModule.Core.Schema;
 using VirtoCommerce.ExperienceApiModule.DigitalCatalog;
 using VirtoCommerce.ExperienceApiModule.XPurchase;
+using VirtoCommerce.ExperienceApiModule.XPurchase.Domain.Factories;
+using VirtoCommerce.ExperienceApiModule.XPurchase.Domain.Services;
+using VirtoCommerce.ExperienceApiModule.XPurchase.Models.Cart.Services;
+using VirtoCommerce.ExperienceApiModule.XPurchase.Models.Marketing.Services;
 using VirtoCommerce.Platform.Core.Modularity;
 namespace VirtoCommerce.ExperienceApiModule.Web
 {
@@ -20,6 +24,11 @@ namespace VirtoCommerce.ExperienceApiModule.Web
             services.AddMediatR(typeof(Anchor));
             services.AddMediatR(typeof(XPurchaseAnchor));
 
+            services.AddTransient<IShoppingCartAggregateFactory, ShoppingCartAggregateFactory>(); 
+            services.AddTransient<ICartService, CartService>();
+            services.AddTransient<ICatalogService, CatalogService>();
+            services.AddTransient<IPromotionEvaluator, PromotionEvaluator>();
+            services.AddTransient<ITaxEvaluator, TaxEvaluator>();
 
             //services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(RequestExceptionProcessorBehavior<,>));
             //serviceCollection.AddSingleton(typeof(IRequestPreProcessor<>), typeof(GenericRequestPreProcessor<>));
