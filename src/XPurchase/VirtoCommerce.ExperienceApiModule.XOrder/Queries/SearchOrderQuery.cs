2c91a069de711e5336c4c62b9f5922196c9415ce
Andrei Kostyrin <kostyrin2@gmail.com>
2020-07-15 21:29:50 +0200

    #VP-3645 clean SearchOrderQuery

--- src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Queries/SearchOrderQuery.cs
+++ src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Queries/SearchOrderQuery.cs
@@ -1,4 +1,3 @@
-using System;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
 
 namespace VirtoCommerce.ExperienceApiModule.XOrder.Queries
@@ -9,23 +8,5 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Queries
         public int Skip { get; set; }
         public int Take { get; set; }
         public string Filter { get; set; }
-
-        //public bool WithPrototypes { get; set; }
-        //public bool OnlyRecurring { get; set; }
-        //public string SubscriptionId { get; set; }
-        //public string[] SubscriptionIds { get; set; }
-        //public string OperationId { get; set; }
-        //public string CustomerId { get; set; }
-        //public string[] CustomerIds { get; set; }
-
-        //public string[] Ids { get; set; }
-        //public string EmployeeId { get; set; }
-        //public string[] StoreIds { get; set; }
-        //public string Status { get; set; }
-        //public string[] Statuses { get; set; }
-        //public string Number { get; set; }
-        //public string[] Numbers { get; set; }
-        //public DateTime? StartDate { get; set; }
-        //public DateTime? EndDate { get; set; }
     }
 }
