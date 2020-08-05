7dfdad624a2755945d728179a5b899d43a1fcda6
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-08-05 08:25:10 +0200

    Fix missing property without values

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Extensions/PropertyExtensions.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Extensions/PropertyExtensions.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.XDigitalCatalog.Extensions
 {
@@ -8,15 +9,24 @@ namespace VirtoCommerce.XDigitalCatalog.Extensions
     {
         public static IList<Property> ExpandByValues(this IEnumerable<Property> properties)
         {
-            return properties
-                .SelectMany(property => property.Values
-                    .Select(propValue =>
-                    {
-                        var result = (Property) property.Clone();
-                        result.Values = new List<PropertyValue> { propValue };
-                        return result;
-                    }))
-                .ToList();
+            var result = properties.SelectMany(property =>
+            {
+                var propertyValues = property.Values.Select(v =>
+                {
+                    var result1 = (Property) property.Clone();
+                    result1.Values = new List<PropertyValue> { v };
+                    return result1;
+                }).ToList();
+
+                if (propertyValues.IsNullOrEmpty())
+                {
+                    propertyValues = new List<Property> { property };
+                }
+
+                return propertyValues;
+            });
+
+            return result.ToArray();
         }
     }
 }
