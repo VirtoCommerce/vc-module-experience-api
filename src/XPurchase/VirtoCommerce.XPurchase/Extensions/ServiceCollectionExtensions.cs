76f84a917b19c9eda12ef4c9f66dc3500eb955d8
alexandr andreev <alivemenone@gmail.com>
2020-08-17 17:38:20 +0300

    VP-4132: Cart XAPI Security (#58)
    * Add security
    
    * Remove SignInManager factory
    
    * Minor refactoring
    
    * remove unused constrains

--- src/XPurchase/VirtoCommerce.XPurchase/Extensions/ServiceCollectionExtensions.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Extensions/ServiceCollectionExtensions.cs
@@ -2,8 +2,12 @@ using System;
 using AutoMapper;
 using GraphQL.Server;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.ExperienceApiModule.Core.Extensions;
+using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.XPurchase.Authorization;
 using VirtoCommerce.XPurchase.Schemas;
 using VirtoCommerce.XPurchase.Services;
 using VirtoCommerce.XPurchase.Validators;
@@ -63,7 +67,7 @@ namespace VirtoCommerce.XPurchase.Extensions
             //services.AddSchemaType<ValidationErrorType>();
 
             services.AddSchemaBuilder<PurchaseSchema>();
-
+            services.AddSingleton<IAuthorizationHandler, CanAccessCartAuthorizationHandler>();
             services.AddTransient<ICartAggregateRepository, CartAggregateRepository>();
 
             services.AddTransient<ICartValidationContextFactory, CartValidationContextFactory>();
