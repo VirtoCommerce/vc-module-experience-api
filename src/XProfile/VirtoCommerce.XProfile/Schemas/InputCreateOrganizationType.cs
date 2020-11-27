3ed81f5fb8489ad2fd609d6e8f3c58c01de08d53
Aleksandr Andreev <alivemenone@gmail.com>
2020-11-27 14:34:45 +0300

    fix sonar issues

--- src/XProfile/VirtoCommerce.XProfile/Schemas/InputCreateOrganizationType.cs
+++ src/XProfile/VirtoCommerce.XProfile/Schemas/InputCreateOrganizationType.cs
@@ -1,6 +1,3 @@
-using GraphQL.Types;
-using VirtoCommerce.CustomerModule.Core.Model;
-
 namespace VirtoCommerce.ExperienceApiModule.XProfile.Schemas
 {
     public class InputCreateOrganizationType : InputMemberBaseType
