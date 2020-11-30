20b99df0ee1cd56347306bd00962a211a0a994e1
alexandr andreev <aad@virtoway.com>
2020-11-30 15:44:11 +0300

    VP-6070: Complete profile authentication and authorization scenarios (#84)
    * update x-profile authorization
    
    * minor refactoring
    
    * fix sonar issues

--- src/XProfile/VirtoCommerce.XProfile/Schemas/InputCreateOrganizationType.cs
+++ src/XProfile/VirtoCommerce.XProfile/Schemas/InputCreateOrganizationType.cs
@@ -1,13 +1,10 @@
-using GraphQL.Types;
-using VirtoCommerce.CustomerModule.Core.Model;
-
 namespace VirtoCommerce.ExperienceApiModule.XProfile.Schemas
 {
     public class InputCreateOrganizationType : InputMemberBaseType
     {
         public InputCreateOrganizationType()
         {
-            Field<NonNullGraphType<StringGraphType>>("userId");
+            
         }
     }
 }
