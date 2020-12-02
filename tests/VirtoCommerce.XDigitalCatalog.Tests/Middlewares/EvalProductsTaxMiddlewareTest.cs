190cd809c63e154a2894d2172da70ca0b45efe0c
Aleksandr Andreev <alivemenone@gmail.com>
2020-12-02 08:52:11 +0300

    Add tax middleware test

--- tests/VirtoCommerce.XDigitalCatalog.Tests/Middlewares/EvalProductsTaxMiddlewareTest.cs
+++ tests/VirtoCommerce.XDigitalCatalog.Tests/Middlewares/EvalProductsTaxMiddlewareTest.cs
@@ -4,6 +4,10 @@ using System.Threading.Tasks;
 using AutoMapper;
 using FluentAssertions;
 using Moq;
+using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.CoreModule.Core.Common;
+using VirtoCommerce.CoreModule.Core.Currency;
+using VirtoCommerce.ExperienceApiModule.Core.Models;
 using VirtoCommerce.ExperienceApiModule.Core.Pipelines;
 using VirtoCommerce.TaxModule.Core.Model;
 using VirtoCommerce.TaxModule.Core.Model.Search;
@@ -19,7 +23,7 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
         [Fact]
         public void EvalProductsTaxMiddleware_TaxNotCalculatedWithoutResponseGroup_Success()
         {
-
+            // Arrange
             var mapper = new Mock<IMapper>();
             var taxProviderSearchService = new Mock<ITaxProviderSearchService>();
             var genericPipelineLauncher = new Mock<IGenericPipelineLauncher>();
@@ -29,24 +33,21 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
             var response = new SearchProductResponse()
             {
                 TotalCount = 1,
-                Results = new List<ExpProduct>()
-                {
-                    new ExpProduct()
-                },
-                Query = new SearchProductQuery()
-                {
-                    CurrencyCode = "USD"
-                }
+                Results = new List<ExpProduct>() {new ExpProduct() },
+                Query = new SearchProductQuery() {CurrencyCode = "USD"}
             };
 
+            //Act
             evalProductsTaxMiddleware.Run(response, resp => Task.CompletedTask);
 
+            // Assert
             taxProviderSearchService.Verify(x=>x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()), Times.Never);
         }
 
         [Fact]
         public void EvalProductsTaxMiddleware_TaxNotCalculatedWithoutTaxProvider_Success()
         {
+            // Arrange
             var mapper = new Mock<IMapper>();
             var taxProviderSearchService = new Mock<ITaxProviderSearchService>();
             taxProviderSearchService.Setup(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()))
@@ -62,10 +63,7 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
             var response = new SearchProductResponse()
             {
                 TotalCount = 1,
-                Results = new List<ExpProduct>()
-                {
-                    new ExpProduct()
-                },
+                Results = new List<ExpProduct>() { new ExpProduct() },
                 Query = new SearchProductQuery()
                 {
                     CurrencyCode = "USD",
@@ -74,8 +72,10 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
                 
             };
 
+            //Act
             Action action = () => evalProductsTaxMiddleware.Run(response, resp => Task.CompletedTask);
 
+            // Assert
             action.Should().NotThrow();
             taxProviderSearchService.Verify(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()), Times.Once);
         }
@@ -83,7 +83,7 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
         [Fact]
         public void EvalProductsTaxMiddleware_TaxRatesCalculated_Success()
         {
-
+            // Arrange
             var taxProvider = new Mock<TaxProvider>();
 
             var mapper = new Mock<IMapper>();
@@ -91,12 +91,12 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
             taxProviderSearchService.Setup(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()))
                 .ReturnsAsync(() => new TaxProviderSearchResult()
                 {
-                    TotalCount = 0,
+                    TotalCount = 1,
                     Results = new List<TaxProvider>() {taxProvider.Object}
                 });
             var genericPipelineLauncher = new Mock<IGenericPipelineLauncher>();
 
-            var q = new EvalProductsTaxMiddleware(mapper.Object, taxProviderSearchService.Object, genericPipelineLauncher.Object);
+            var evalProductsTaxMiddleware = new EvalProductsTaxMiddleware(mapper.Object, taxProviderSearchService.Object, genericPipelineLauncher.Object);
 
             var response = new SearchProductResponse()
             {
@@ -113,12 +113,68 @@ namespace VirtoCommerce.XDigitalCatalog.Tests.Middlewares
 
             };
 
-            Action action = () => q.Run(response, resp => Task.CompletedTask);
+            //Act
+            Action action = () => evalProductsTaxMiddleware.Run(response, resp => Task.CompletedTask);
 
+            // Assert
             action.Should().NotThrow();
             taxProviderSearchService.Verify(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()), Times.Once);
             taxProvider.Verify(x=>x.CalculateRates(It.IsAny<TaxEvaluationContext>()), Times.Once);
         }
 
+        [Fact]
+        public void EvalProductsTaxMiddleware_TaxesApply_Success()
+        {
+            // Arrange
+            var currency = new Currency(Language.InvariantLanguage, "USD");
+
+            var productPrice = new Mock<ProductPrice>(currency).Object;
+            productPrice.TaxPercentRate = 0;
+            productPrice.ListPrice = new Money(100m, currency);
+            productPrice.DiscountAmount = new Money(0m, currency);
+            productPrice.ProductId = "someId";
+
+            var taxProvider = new Mock<TaxProvider>();
+            taxProvider.Setup(x => x.CalculateRates(It.IsAny<TaxEvaluationContext>()))
+                .Returns(() => new List<TaxRate>() {new TaxRate() {Currency = "USD", Rate = 50, Line = new TaxLine() {Id = "someId", Quantity = 0}}});
+
+            var mapper = new Mock<IMapper>();
+            var taxProviderSearchService = new Mock<ITaxProviderSearchService>();
+            taxProviderSearchService.Setup(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()))
+                .ReturnsAsync(() => new TaxProviderSearchResult()
+                {
+                    TotalCount = 0,
+                    Results = new List<TaxProvider>() { taxProvider.Object }
+                });
+            var genericPipelineLauncher = new Mock<IGenericPipelineLauncher>();
+
+            var evalProductsTaxMiddleware = new EvalProductsTaxMiddleware(mapper.Object, taxProviderSearchService.Object, genericPipelineLauncher.Object);
+
+            var response = new SearchProductResponse()
+            {
+                TotalCount = 1,
+                Results = new List<ExpProduct>()  {
+                    new ExpProduct()
+                    {
+                       
+                        AllPrices = new List<ProductPrice>()
+                        {
+                            productPrice
+                        }
+
+                    }
+                },
+                Query = new SearchProductQuery() {CurrencyCode = "USD", IncludeFields = new List<string>() { "price" }},
+            };
+
+            //Act
+            Action action = () => evalProductsTaxMiddleware.Run(response, resp => Task.CompletedTask);
+
+            // Assert
+            action.Should().NotThrow();
+            taxProviderSearchService.Verify(x => x.SearchTaxProvidersAsync(It.IsAny<TaxProviderSearchCriteria>()), Times.Once);
+            taxProvider.Verify(x => x.CalculateRates(It.IsAny<TaxEvaluationContext>()), Times.Once);
+            productPrice.TaxPercentRate.Should().Be(0.5m);
+        }
     }
 }
