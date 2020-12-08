bef2d0872fe3801f301cb8d7f48b4d020bdc0560
Aleksandr Andreev <alivemenone@gmail.com>
2020-12-08 13:39:53 +0300

    revert changes

--- src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Schemas/OrderPaymentMethodType.cs
+++ src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Schemas/OrderPaymentMethodType.cs
@@ -1,3 +1,4 @@
+using GraphQL;
 using GraphQL.Types;
 using VirtoCommerce.CoreModule.Core.Currency;
 using VirtoCommerce.ExperienceApiModule.Core.Schemas;
@@ -12,24 +13,24 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Schemas
         {
             Field<ListGraphType<OrderTaxDetailType>>(nameof(PaymentMethod.TaxDetails), resolve: x => x.Source.TaxDetails);
             Field(x => x.TaxPercentRate);
-            Field(x => x.TaxTotal);
+            Field<MoneyType>(nameof(PaymentMethod.TaxTotal).ToCamelCase(), resolve: context => new Money(context.Source.TaxTotal, context.GetOrderCurrency()));
             Field(x => x.TaxType, true);
             Field(x => x.TypeName);
             Field(x => x.StoreId);
-            Field(x => x.DiscountAmountWithTax);
-            Field(x => x.DiscountAmount);
-            Field(x => x.TotalWithTax);
-            Field(x => x.Total);
-            Field(x => x.PriceWithTax);
-            Field(x => x.Price);
-            Field(x => x.Currency, true);
+            Field<MoneyType>(nameof(PaymentMethod.TotalWithTax).ToCamelCase(), resolve: context => new Money(context.Source.TotalWithTax, context.GetOrderCurrency()));
+            Field<MoneyType>(nameof(PaymentMethod.Total).ToCamelCase(), resolve: context => new Money(context.Source.Total, context.GetOrderCurrency()));
+            Field<MoneyType>(nameof(PaymentMethod.DiscountAmount).ToCamelCase(), resolve: context => new Money(context.Source.DiscountAmount, context.GetOrderCurrency()));
+            Field<MoneyType>(nameof(PaymentMethod.DiscountAmountWithTax).ToCamelCase(), resolve: context => new Money(context.Source.DiscountAmountWithTax, context.GetOrderCurrency()));
+            Field<MoneyType>(nameof(PaymentMethod.Price).ToCamelCase(), resolve: context => new Money(context.Source.Price, context.GetOrderCurrency()));
+            Field<MoneyType>(nameof(PaymentMethod.PriceWithTax).ToCamelCase(), resolve: context => new Money(context.Source.PriceWithTax, context.GetOrderCurrency()));
+            Field<CurrencyType>(nameof(PaymentMethod.Currency).ToCamelCase(), resolve: context => context.GetOrderCurrency());
             Field(x => x.IsAvailableForPartial);
             Field(x => x.Priority);
             Field(x => x.IsActive);
             Field(x => x.LogoUrl);
             Field(x => x.Code);
-            Field<IntGraphType>(nameof(PaymentMethod.PaymentMethodType), resolve: context => (int)context.Source.PaymentMethodType);
-            Field<IntGraphType>(nameof(PaymentMethod.PaymentMethodGroupType), resolve: context => (int)context.Source.PaymentMethodGroupType);
+            Field<IntGraphType>(nameof(PaymentMethod.PaymentMethodType), resolve: context => (int) context.Source.PaymentMethodType);
+            Field<IntGraphType>(nameof(PaymentMethod.PaymentMethodGroupType), resolve: context => (int) context.Source.PaymentMethodGroupType);
 
             //TODO
             //public ICollection<ObjectSettingEntry> Settings);
