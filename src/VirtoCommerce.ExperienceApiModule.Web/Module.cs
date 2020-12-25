ad06960d5b1b3f9fb73603bccf4921d3a0ddcb22
alexandr andreev <aad@virtoway.com>
2020-12-25 11:00:58 +0300

    VP-6356: DateTime for GraphQL schema returns only date (#105)
    * Map DateTime to DateTimeGraphType
    
    * Add TODO

--- src/VirtoCommerce.ExperienceApiModule.Web/Module.cs
+++ src/VirtoCommerce.ExperienceApiModule.Web/Module.cs
@@ -1,13 +1,14 @@
+using System;
 using AutoMapper;
 using GraphQL.Server;
 using GraphQL.Types;
+using GraphQL.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.ExperienceApiModule.Core;
 using VirtoCommerce.ExperienceApiModule.Core.Extensions;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
 using VirtoCommerce.ExperienceApiModule.Core.Pipelines;
-using VirtoCommerce.ExperienceApiModule.Core.Schemas;
 using VirtoCommerce.ExperienceApiModule.XOrder.Extensions;
 using VirtoCommerce.ExperienceApiModule.XProfile.Extensions;
 using VirtoCommerce.MarketingModule.Core.Model.Promotions;
@@ -49,6 +50,10 @@ namespace VirtoCommerce.ExperienceApiModule.Web
             services.AddXProfile(graphQlBuilder);
             services.AddXPurchase(graphQlBuilder);
             services.AddXOrder(graphQlBuilder);
+
+            //TODO: Remove after update GraphQL.net to 3.2.0 version.
+            //VP-6356 DateTime field types for GraphQL schema do not return time in result
+            GraphTypeTypeRegistry.Register<DateTime, DateTimeGraphType>();
 
 
             services.AddSingleton<IStoreCurrencyResolver, StoreCurrencyResolver>();
