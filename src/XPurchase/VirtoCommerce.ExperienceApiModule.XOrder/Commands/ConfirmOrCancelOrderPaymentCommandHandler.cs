6d3be2a63f4b329091ce457b279367567ec4e193
Aleksandr Andreev <alivemenone@gmail.com>
2021-03-02 14:29:33 +0300

    Revert "Fix the issue with setting any payment status when call confirmOrderPayment or cancelOrderPayment"
    This reverts commit 9a4d3d5ce1c48952937712e4342a44902f97be09.

--- src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Commands/ConfirmOrCancelOrderPaymentCommandHandler.cs
+++ src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Commands/ConfirmOrCancelOrderPaymentCommandHandler.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -18,25 +19,19 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Commands
         public async Task<bool> Handle(CancelOrderPaymentCommand request, CancellationToken cancellationToken)
         {
             var orderAggregate = await _customerOrderAggregateRepository.GetOrderByIdAsync(request.Payment.OrderId);
-            var paymentCancelledSuccessful = orderAggregate.CancelOrderPayment(request.Payment);
-            if (paymentCancelledSuccessful)
-            {
-                await _customerOrderService.SaveChangesAsync(new[] { orderAggregate.Order });
-            }
+            orderAggregate.CancelOrderPayment(request.Payment);
 
-            return paymentCancelledSuccessful;
+            await _customerOrderService.SaveChangesAsync(new[] { orderAggregate.Order });
+
+            return true;
         }
 
         public async Task<bool> Handle(ConfirmOrderPaymentCommand request, CancellationToken cancellationToken)
         {
             var orderAggregate = await _customerOrderAggregateRepository.GetOrderByIdAsync(request.Payment.OrderId);
-            var paymentConfirmedSuccessful = orderAggregate.ConfirmOrderPayment(request.Payment);
-            if (paymentConfirmedSuccessful)
-            {
-                await _customerOrderService.SaveChangesAsync(new[] { orderAggregate.Order });
-            }
-
-            return paymentConfirmedSuccessful;
+            orderAggregate.ConfirmOrderPayment(request.Payment);
+            await _customerOrderService.SaveChangesAsync(new[] { orderAggregate.Order });
+            return true;
         }
     }
 }
