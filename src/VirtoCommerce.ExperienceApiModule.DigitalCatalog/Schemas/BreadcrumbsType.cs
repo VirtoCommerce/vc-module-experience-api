ebc9bb66ba95f7bc6b7d75bfc1235d72583452b1
Aleksandr Andreev <alivemenone@gmail.com>
2021-03-09 15:27:22 +0300

    Remover URL field

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/BreadcrumbsType.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/BreadcrumbsType.cs
@@ -12,7 +12,6 @@ namespace VirtoCommerce.XDigitalCatalog.Schemas
             Field(x => x.Title).Description("Name of current breadcrumb");
             Field(x => x.TypeName, nullable: true).Description("Catalog, category or product");
             Field(x => x.SeoPath, nullable: true).Description("Full path from catalog");
-            Field(x => x.Url, nullable: true).Description("Url of the breadcrumb");
         }
     }
 }
