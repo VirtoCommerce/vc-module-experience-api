3b797d4d649409a271714e15012f811eacb7be6e
Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>
2021-05-05 15:20:37 +0200

    PT-1624: Check todos (#148)

--- src/VirtoCommerce.ExperienceApiModule.Core/Schemas/CurrencyType.cs
+++ src/VirtoCommerce.ExperienceApiModule.Core/Schemas/CurrencyType.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using GraphQL.Types;
 using VirtoCommerce.CoreModule.Core.Currency;
 
@@ -11,11 +8,7 @@ namespace VirtoCommerce.ExperienceApiModule.Core.Schemas
         public CurrencyType()
         {
             Field(x => x.Code, nullable: false).Description("Currency code may be used ISO 4217");
-            //TODO:
-            //Field(x => x.CultureName, nullable: false).Description("Culture name");
             Field(x => x.Symbol, nullable: true).Description("Symbol");
-            //TODO:
-            //Field(x => x.EnglishName, nullable: false).Description("English name");
             Field(x => x.ExchangeRate, nullable: true).Description("Exchange rate");
             Field(x => x.CustomFormatting, nullable: true).Description("Currency custom formatting");
         }
