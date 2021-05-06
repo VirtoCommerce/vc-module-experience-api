9b9df4d69af05eebd15efd9b1955b62d89656135
Eugeny Tatarincev <et@virtoway.com>
2021-05-06 17:49:50 +0200

    add OverrideQueryType, WithQueryHandler extension methods

---
+++ samples/VirtoCommerce.Exp.ExtensionSamples/UseCases/TypeExtension/Queries/GetCartQueryExtended.cs
@@ -0,0 +1,9 @@
+using VirtoCommerce.XPurchase.Queries;
+
+namespace VirtoCommerce.Exp.ExtensionSamples.UseCases.TypeExtension.Queries
+{
+    public class GetCartQueryExtended : GetCartQuery
+    {
+        //define custom properties here
+    }
+}
