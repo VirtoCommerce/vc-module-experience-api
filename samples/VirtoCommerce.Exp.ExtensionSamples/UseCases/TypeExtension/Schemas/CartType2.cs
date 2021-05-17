7f7b7b48af9314a76d538f8701793b93a0632a98
Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>
2021-05-17 15:51:08 +0200

    Revert "Revert "PT-1104 add Dynamic properties to Cart types (#154)" (#156)"
    This reverts commit 4335ff2027ca484c2a34894bce6fd5bbc8cddd03.

--- samples/VirtoCommerce.Exp.ExtensionSamples/UseCases/TypeExtension/Schemas/CartType2.cs
+++ samples/VirtoCommerce.Exp.ExtensionSamples/UseCases/TypeExtension/Schemas/CartType2.cs
@@ -1,4 +1,5 @@
 using GraphQL.Types;
+using VirtoCommerce.ExperienceApiModule.Core.Services;
 using VirtoCommerce.XPurchase.Schemas;
 using VirtoCommerce.XPurchase.Services;
 
@@ -6,9 +7,10 @@ namespace VirtoCommerce.Exp.ExtensionSamples
 {
     public class CartType2 : CartType
     {
-        public CartType2(ICartAvailMethodsService cartAvailMethods) : base(cartAvailMethods)
+        public CartType2(ICartAvailMethodsService cartAvailMethods, IDynamicPropertyResolverService dynamicPropertyResolver)
+            : base(cartAvailMethods, dynamicPropertyResolver)
         {
-            Field<StringGraphType>("myCoolScalarProperty", resolve: context => "my cool value" );
+            Field<StringGraphType>("myCoolScalarProperty", resolve: context => "my cool value");
         }
     }
 }
