d71a623eeb2efd50060f7859b8a98a53016d6cce
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-05-20 11:27:56 +0200

    PT-1846 removed code duplication for dynamic properties read and update

--- src/XProfile/VirtoCommerce.XProfile/Aggregates/MemberAggregateRootBase.cs
+++ src/XProfile/VirtoCommerce.XProfile/Aggregates/MemberAggregateRootBase.cs
@@ -1,10 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using VirtoCommerce.CustomerModule.Core.Model;
-using VirtoCommerce.ExperienceApiModule.Core.Models;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Core.DynamicProperties;
 
 namespace VirtoCommerce.ExperienceApiModule.XProfile.Aggregates
 {
@@ -17,73 +13,6 @@ namespace VirtoCommerce.ExperienceApiModule.XProfile.Aggregates
             Member.Addresses = addresses.ToList();
 
             return this;
-        }
-
-        public virtual async Task<MemberAggregateRootBase> UpdateDynamicPropertiesAsync(IList<DynamicPropertyValue> values, IDynamicPropertyMetaDataResolver metaDataResolver)
-        {
-            var tasks = values.GroupBy(x => x.Name).Select(async newValuesGroup =>
-            {
-                var result = AbstractTypeFactory<DynamicObjectProperty>.TryCreateInstance();
-                result.Name = newValuesGroup.Key;
-
-                var metadata = await metaDataResolver.GetByNameAsync(Member.ObjectType, newValuesGroup.Key);
-                if (metadata != null)
-                {
-                    result.SetMetaData(metadata);
-                }
-
-                if (result.IsDictionary)
-                {
-                    // all Values actually are IDs of dictionary values => set ValueIds
-                    foreach (var propValue in newValuesGroup.Where(x => x.Value is string))
-                    {
-                        propValue.ValueId = (string)propValue.Value;
-                    }
-                }
-
-                foreach (var newValue in newValuesGroup)
-                {
-                    newValue.ValueType = result.ValueType;
-                }
-
-                result.Values = newValuesGroup.ToArray();
-                return result;
-            });
-            var sourceProperties = await Task.WhenAll(tasks);
-
-            var comparer = AnonymousComparer.Create((DynamicObjectProperty x) => x.Name);
-
-            // fill missing values with the original ones. (That enables single value updates)
-            sourceProperties = sourceProperties.Union(Member.DynamicProperties, comparer).ToArray();
-
-            Member.DynamicProperties = Member.DynamicProperties.ToList();
-            sourceProperties.Patch(Member.DynamicProperties, comparer, Patch);
-
-            return this;
-        }
-
-        private void Patch(DynamicObjectProperty source, DynamicObjectProperty target)
-        {
-            // override only values of a specific locale for multilingual property. Except dictionary properties.
-            if (source.IsMultilingual && !source.IsDictionary)
-            {
-                var comparer = AnonymousComparer.Create((DynamicPropertyObjectValue x) => x.Locale);
-
-                // fill missing values with the original ones. (That enables single value updates)
-                source.Values = source.Values.Union(target.Values, comparer).ToArray();
-
-                target.Values = target.Values.ToList();
-                source.Values.Patch(target.Values, comparer, Patch);
-            }
-            else
-            {
-                target.Values = source.Values;
-            }
-        }
-
-        private void Patch(DynamicPropertyObjectValue source, DynamicPropertyObjectValue target)
-        {
-            target.Value = source.Value;
         }
     }
 }
--- src/XProfile/VirtoCommerce.XProfile/Commands/UpdateMemberDynamicPropertiesCommandHandler.cs
+++ src/XProfile/VirtoCommerce.XProfile/Commands/UpdateMemberDynamicPropertiesCommandHandler.cs
@@ -1,28 +1,27 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using VirtoCommerce.ExperienceApiModule.Core.Services;
 using VirtoCommerce.ExperienceApiModule.XProfile.Aggregates;
-using VirtoCommerce.Platform.Core.DynamicProperties;
 
 namespace VirtoCommerce.ExperienceApiModule.XProfile.Commands
 {
     public class UpdateMemberDynamicPropertiesCommandHandler : IRequestHandler<UpdateMemberDynamicPropertiesCommand, IMemberAggregateRoot>
     {
         private readonly IContactAggregateRepository _contactAggregateRepository;
-        private readonly IDynamicPropertyMetaDataResolver _metadataResolver;
+        private readonly IDynamicPropertyUpdaterService _dynamicPropertyUpdater;
 
-
-        public UpdateMemberDynamicPropertiesCommandHandler(IContactAggregateRepository contactAggregateRepository, IDynamicPropertyMetaDataResolver metadataResolver)
+        public UpdateMemberDynamicPropertiesCommandHandler(IContactAggregateRepository contactAggregateRepository, IDynamicPropertyUpdaterService dynamicPropertyUpdater)
         {
             _contactAggregateRepository = contactAggregateRepository;
-            _metadataResolver = metadataResolver;
+            _dynamicPropertyUpdater = dynamicPropertyUpdater;
         }
 
         public async Task<IMemberAggregateRoot> Handle(UpdateMemberDynamicPropertiesCommand request, CancellationToken cancellationToken)
         {
             var memberAggregate = await _contactAggregateRepository.GetMemberAggregateRootByIdAsync<MemberAggregateRootBase>(request.MemberId);
 
-            await memberAggregate.UpdateDynamicPropertiesAsync(request.DynamicProperties, _metadataResolver);
+            await _dynamicPropertyUpdater.UpdateDynamicPropertyValues(memberAggregate.Member, request.DynamicProperties);
 
             await _contactAggregateRepository.SaveAsync(memberAggregate);
 
