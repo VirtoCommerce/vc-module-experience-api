375c013c78f737ca8ae420742c41edf0cdf473dd
Andrei Kolchanov <akak1977@users.noreply.github.com>
2021-07-12 19:33:33 +0500

    PT-2640: Add SemanticUrl to Breadcrumb (#173)

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/BreadcrumbsType.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/BreadcrumbsType.cs
@@ -12,6 +12,7 @@ namespace VirtoCommerce.XDigitalCatalog.Schemas
             Field(x => x.Title).Description("Name of current breadcrumb");
             Field(x => x.TypeName, nullable: true).Description("Catalog, category or product");
             Field(x => x.SeoPath, nullable: true).Description("Full path from catalog");
+            Field(x => x.SemanticUrl, nullable: true).Description("Semantic URL keyword");
         }
     }
 }
