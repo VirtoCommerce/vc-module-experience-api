fa4f19685c7b853210c8cb24526472d3c3992bec
Egis <em@virtoway.com>
2021-08-20 14:16:24 +0300

    PT-3537: Add RemoveNullCatalogProductsMiddleware (#196)

---
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Middlewares/RemoveNullCatalogProductsMiddleware.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using PipelineNet.Middleware;
+using VirtoCommerce.XDigitalCatalog.Queries;
+
+namespace VirtoCommerce.XDigitalCatalog.Middlewares
+{
+    public class RemoveNullCatalogProductsMiddleware : IAsyncMiddleware<SearchProductResponse>
+    {
+        public Task Run(SearchProductResponse parameter, Func<SearchProductResponse, Task> next)
+        {
+            var missingItems = parameter.Results
+                .Where(expProduct => expProduct.IndexedProduct is null)
+                .ToArray();
+
+            foreach (var missingItem in missingItems)
+            {
+                parameter.Results.Remove(missingItem);
+            }
+
+            parameter.TotalCount -= missingItems.Length;
+
+            return next(parameter);
+        }
+    }
+}
