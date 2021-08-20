93499eaa9d6141a254a68a2c545b38df0678c163
Egidijus Mazeika <em@virtoway.com>
2021-08-20 11:53:19 +0300

    VP-0 Remove unused parameter

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Middlewares/RemoveNullCatalogProductsMiddleware.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Middlewares/RemoveNullCatalogProductsMiddleware.cs
@@ -2,20 +2,12 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using PipelineNet.Middleware;
-using VirtoCommerce.CatalogModule.Core.Services;
 using VirtoCommerce.XDigitalCatalog.Queries;
 
 namespace VirtoCommerce.XDigitalCatalog.Middlewares
 {
     public class RemoveNullCatalogProductsMiddleware : IAsyncMiddleware<SearchProductResponse>
     {
-        private readonly IItemService _itemService;
-
-        public RemoveNullCatalogProductsMiddleware(IItemService itemService)
-        {
-            _itemService = itemService;
-        }
-
         public Task Run(SearchProductResponse parameter, Func<SearchProductResponse, Task> next)
         {
             var missingItems = parameter.Results
