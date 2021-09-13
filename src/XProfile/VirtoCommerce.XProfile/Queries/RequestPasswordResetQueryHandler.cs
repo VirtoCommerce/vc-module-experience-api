811fea8183ee151ead01a7c7624adf916a93d518
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-09-13 15:25:14 +0200

    Set user id and token as query parameters

--- src/XProfile/VirtoCommerce.XProfile/Queries/RequestPasswordResetQueryHandler.cs
+++ src/XProfile/VirtoCommerce.XProfile/Queries/RequestPasswordResetQueryHandler.cs
@@ -48,7 +48,7 @@ namespace VirtoCommerce.ExperienceApiModule.XProfile.Queries
                     var token = await userManager.GeneratePasswordResetTokenAsync(user);
 
                     var notification = await _notificationSearchService.GetNotificationAsync<ResetPasswordEmailNotification>();
-                    notification.Url = $"{store.Url.TrimLastSlash()}{request.UrlSuffix.NormalizeUrlSuffix()}/{user.Id}/{token}";
+                    notification.Url = $"{store.Url.TrimLastSlash()}{request.UrlSuffix.NormalizeUrlSuffix()}?userId={user.Id}&token={token}";
                     notification.To = user.Email;
                     notification.From = store.Email;
 
