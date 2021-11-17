50ca121f751beb5ecadde1f0f070d19d9a257446
zashchitnik-kuka <91291746+zashchitnik-kuka@users.noreply.github.com>
2021-11-17 04:39:26 +0300

    docs: Fix linguistics

--- src/XPurchase/VirtoCommerce.XPurchase/Schemas/InputPaymentType.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/InputPaymentType.cs
@@ -7,9 +7,9 @@ namespace VirtoCommerce.XPurchase.Schemas
     {
         public InputPaymentType()
         {
-            Field(x => x.Id, nullable: true).Description("The payment Id");
-            Field(x => x.OuterId, nullable: true).Description("Value of payment outer Id");
-            Field(x => x.PaymentGatewayCode, nullable: true).Description("Value of payment gateway code");
+            Field(x => x.Id, nullable: true).Description("Payment ID");
+            Field(x => x.OuterId, nullable: true).Description("Payment outer ID value");
+            Field(x => x.PaymentGatewayCode, nullable: true).Description("Payment gateway code value");
             Field<InputAddressType>("billingAddress");
             Field(x => x.Currency, nullable: true);
             Field(x => x.Price, nullable: true);
