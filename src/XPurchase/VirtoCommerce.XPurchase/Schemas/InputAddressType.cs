1da3a8b7a8e82c0da37d6b2cba9c4be8bf77dd93
zashchitnik-kuka <91291746+zashchitnik-kuka@users.noreply.github.com>
2021-11-17 04:38:01 +0300

    docs: Fix linguistics

--- src/XPurchase/VirtoCommerce.XPurchase/Schemas/InputAddressType.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/InputAddressType.cs
@@ -7,13 +7,13 @@ namespace VirtoCommerce.XPurchase.Schemas
     {
         public InputAddressType()
         {
-            Field<StringGraphType>("id", resolve: context => context.Source.Key, description: "Id");
+            Field<StringGraphType>("id", resolve: context => context.Source.Key, description: "ID");
             Field(x => x.City, nullable: true).Description("City");
             Field(x => x.CountryCode, nullable: true).Description("Country code");
-            Field(x => x.CountryName, nullable: true).Description("Country name");
+            Field(x => x.CountryName, nullable: true).Description("Country");
             Field(x => x.Email, nullable: true).Description("Email");
             Field(x => x.FirstName, nullable: true).Description("First name");
-            Field(x => x.Key, nullable: true).Description("Id");
+            Field(x => x.Key, nullable: true).Description("ID");
             Field(x => x.LastName, nullable: true).Description("Last name");
             Field(x => x.Line1, nullable: true).Description("Line1");
             Field(x => x.Line2, nullable: true).Description("Line2");
@@ -22,10 +22,10 @@ namespace VirtoCommerce.XPurchase.Schemas
             Field(x => x.Organization, nullable: true).Description("Company name");
             Field(x => x.Phone, nullable: true).Description("Phone");
             Field(x => x.PostalCode, nullable: true).Description("Postal code");
-            Field(x => x.RegionId, nullable: true).Description("Region Id");
-            Field(x => x.RegionName, nullable: true).Description("Region name");
+            Field(x => x.RegionId, nullable: true).Description("Region ID");
+            Field(x => x.RegionName, nullable: true).Description("Region");
             Field(x => x.Zip, nullable: true).Description("Zip");
-            Field(x => x.OuterId, nullable: true).Description("Outer Id");
+            Field(x => x.OuterId, nullable: true).Description("Outer ID");
             Field<IntGraphType>(nameof(Address.AddressType));
         }
     }
