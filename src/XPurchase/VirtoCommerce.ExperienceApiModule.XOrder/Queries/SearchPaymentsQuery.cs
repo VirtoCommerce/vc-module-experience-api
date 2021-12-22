eb5e128f403a3f26fe1aa65970cfc5a4393322f8
BelyakovSergey <88537215+BelyakovSergey@users.noreply.github.com>
2021-12-22 10:32:53 +0300

    PT-5954: Can't extend Orders connection handler in XAPI (#259)
    * PT-5954: Can't extend Orders connection handler in XAPI
    
    * PT-5954: Replace with extended methods
    
    * PT-5954: Move to Core project
    
    * PT-5954: Replace by IResolveFieldContext
    
    * PT-5954: fix bug

--- src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Queries/SearchPaymentsQuery.cs
+++ src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Queries/SearchPaymentsQuery.cs
@@ -1,9 +1,14 @@
+using System;
+using GraphQL;
+using GraphQL.Builders;
+using VirtoCommerce.CoreModule.Core.Currency;
+using VirtoCommerce.ExperienceApiModule.Core.Extensions;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
 using VirtoCommerce.OrdersModule.Core.Model.Search;
 
 namespace VirtoCommerce.ExperienceApiModule.XOrder.Queries
 {
-    public class SearchPaymentsQuery : IQuery<PaymentSearchResult>
+    public class SearchPaymentsQuery : IQuery<PaymentSearchResult>, IExtendableQuery
     {
         public string Sort { get; set; }
         public int Skip { get; set; }
@@ -11,5 +16,16 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Queries
         public string Filter { get; set; }
         public string CultureName { get; set; }
         public string CustomerId { get; set; }
+
+        public virtual void Map(IResolveFieldContext context)
+        {
+            var connectionContext = (IResolveConnectionContext)context;
+            Skip = Convert.ToInt32(connectionContext.After ?? 0.ToString());
+            Take = connectionContext.First ?? connectionContext.PageSize ?? 10;
+            CultureName = connectionContext.GetArgument<string>(nameof(Currency.CultureName).ToCamelCase());
+            Filter = connectionContext.GetArgument<string>("filter");
+            Sort = connectionContext.GetArgument<string>("sort");
+            CustomerId = connectionContext.GetArgumentOrValue<string>("userId");
+        }
     }
 }
