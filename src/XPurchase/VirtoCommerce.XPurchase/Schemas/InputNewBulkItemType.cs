27e3faffa53ad9df64d2574c09ed709ef6ac336a
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2022-02-21 13:10:41 +0200

    feat: Add addBulkItemsCart mutation

---
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/InputNewBulkItemType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace VirtoCommerce.XPurchase.Schemas
+{
+    public class InputNewBulkItemType : InputObjectGraphType<NewBulkCartItem>
+    {
+        public InputNewBulkItemType()
+        {
+            Field(x => x.ProductSku, nullable: false).Description("Product SKU");
+            Field(x => x.Quantity, nullable: true).Description("Product quantity");
+        }
+    }
+}
