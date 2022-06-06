447e437fadca98b1f647ccd82218e41d91c773de
Leysan Nigmatullina <leysunny21@gmail.com>
2022-06-06 15:43:14 +0300

    PT-7587: Fix resolve 'listPrice' in addItemsCart mutation

--- src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
@@ -946,7 +946,10 @@ namespace VirtoCommerce.XPurchase.Schemas
 
                                                      await CheckAuthByCartCommandAsync(context, cartCommand);
 
-                                                     return await _mediator.Send(cartCommand);
+                                                     var cartAggregate = await _mediator.Send(cartCommand);
+
+                                                     context.SetExpandedObjectGraph(cartAggregate);
+                                                     return cartAggregate;
                                                  })
                                                  .FieldType;
 
