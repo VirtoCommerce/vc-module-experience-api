439fc8cd1731a610624adff314239b07fafcf072
OlegoO <zhukoo@gmail.com>
2022-09-29 19:02:41 +0200

    fix: error when creating an order with product with list/sale price

--- src/XPurchase/VirtoCommerce.XPurchase/Validators/CartLineItemValidator.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Validators/CartLineItemValidator.cs
@@ -27,9 +27,9 @@ namespace VirtoCommerce.XPurchase.Validators
                     }
 
                     var tierPrice = cartProduct.Price.GetTierPrice(lineItem.Quantity);
-                    if (tierPrice.Price.Amount != lineItem.SalePrice)
+                    if (tierPrice.ActualPrice.Amount != lineItem.SalePrice)
                     {
-                        context.AddFailure(CartErrorDescriber.ProductPriceChangedError(lineItem, lineItem.SalePrice, lineItem.SalePriceWithTax, tierPrice.Price.Amount, tierPrice.PriceWithTax.Amount));
+                        context.AddFailure(CartErrorDescriber.ProductPriceChangedError(lineItem, lineItem.SalePrice, lineItem.SalePriceWithTax, tierPrice.ActualPrice.Amount, tierPrice.ActualPriceWithTax.Amount));
                     }
 
                     var minQuantity = cartProduct?.Product?.MinQuantity;
