297e65e9903258aa79dcf1ace06a44214374c647
Andrey Kolchanov <ak@ak.ak>
2022-10-18 15:22:29 +0500

    fix: Remove empty lines

--- src/VirtoCommerce.ExperienceApiModule.Core/Infrastructure/Authorization/ForbiddenError.cs
+++ src/VirtoCommerce.ExperienceApiModule.Core/Infrastructure/Authorization/ForbiddenError.cs
@@ -8,10 +8,7 @@ namespace VirtoCommerce.ExperienceApiModule.Core.Infrastructure.Authorization
     public class ForbiddenError : ExecutionError
     {
         public ForbiddenError(string message) : base(message) => Code = Constants.ForbiddenCode;
-
         public ForbiddenError(string message, IDictionary data) : base(message, data) => Code = Constants.ForbiddenCode;
-
         public ForbiddenError(string message, Exception exception) : base(message, exception) => Code = Constants.ForbiddenCode;
-
     }
 }
