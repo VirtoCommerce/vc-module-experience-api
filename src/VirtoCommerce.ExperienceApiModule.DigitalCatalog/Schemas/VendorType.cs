c42ad40c565c9973b10637a7a38134ae2392968a
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2022-11-25 06:23:33 +0200

    fix: invert customer review dependency

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/VendorType.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/VendorType.cs
@@ -1,9 +1,5 @@
 using MediatR;
-using VirtoCommerce.ExperienceApiModule.Core.Extensions;
-using VirtoCommerce.ExperienceApiModule.Core.Helpers;
 using VirtoCommerce.ExperienceApiModule.Core.Schemas;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.XDigitalCatalog.Queries;
 
 namespace VirtoCommerce.XDigitalCatalog.Schemas;
 
@@ -15,20 +11,5 @@ public class VendorType: ExtendableGraphType<ExpVendorType>
 
         Field(x => x.Id, nullable: false).Description("Vendor ID");
         Field(x => x.Name, nullable: false).Description("Vendor name");
-
-        FieldAsync(
-            GraphTypeExtenstionHelper.GetActualType<RatingType>(),
-            "rating",
-            "Vendor rating",
-            resolve: async context =>
-            {
-                var storeId = context.GetArgumentOrValue<string>("storeId");
-                var query = AbstractTypeFactory<GetRatingQuery>.TryCreateInstance();
-                query.StoreId = storeId;
-                query.EntityId = context.Source.Id;
-                query.EntityType = context.Source.Type;
-                var result = await mediator.Send(query);
-                return result;
-            });
     }
 }
