7c6fe3f187c9f41d26402e4813a671efd042c72b
Evgeny Okhrimenko <evgokhrimenko@gmail.com>
2023-01-11 13:12:40 +0200

    PT-10134: Add vendor in payment/shipment (#365)
    * Add vendor in payment/shipment
    
    * update nugets and year

---
+++ src/VirtoCommerce.ExperienceApiModule.Core/ExpVendor.cs
@@ -0,0 +1,13 @@
+namespace VirtoCommerce.ExperienceApiModule.Core
+{
+    public class ExpVendor
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public ExpRating Rating { get; set; }
+    }
+}
--- src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Models/ExpOrderPayment.cs
+++ src/XPurchase/VirtoCommerce.ExperienceApiModule.XOrder/Models/ExpOrderPayment.cs
@@ -14,6 +14,7 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Models
         public Optional<string> Currency { get; set; }
         public Optional<decimal> Price { get; set; }
         public Optional<decimal> Amount { get; set; }
+        public Optional<string> VendorId { get; set; }
 
         public Optional<ExpOrderAddress> BillingAddress { get; set; }
 
@@ -51,6 +52,11 @@ namespace VirtoCommerce.ExperienceApiModule.XOrder.Models
             if (Amount?.IsSpecified == true)
             {
                 payment.Sum = Amount.Value;
+            }
+
+            if (VendorId?.IsSpecified == true)
+            {
+                payment.VendorId = VendorId.Value;
             }
 
             if (BillingAddress?.IsSpecified == true)
