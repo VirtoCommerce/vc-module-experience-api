20035b7273f367468b358b433e1e6a994ae9acf0
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-01-12 13:50:18 +0200

    PT-10315: fix add to cart error (#367)

--- src/XPurchase/VirtoCommerce.XPurchase/Services/CartProductService.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Services/CartProductService.cs
@@ -28,7 +28,17 @@ namespace VirtoCommerce.XPurchase.Services
         /// Default response group
         /// </summary>
         protected virtual ItemResponseGroup ResponseGroups => ItemResponseGroup.ItemAssets | ItemResponseGroup.ItemInfo | ItemResponseGroup.Outlines | ItemResponseGroup.Seo;
-        protected virtual string[] IncludeFields => new string[] { "__object", "price", "availabilityData" };
+        protected virtual string[] IncludeFields => new string[]
+        {
+            "__object",
+            "price",
+            "availabilityData",
+            "images",
+            "properties",
+            "description",
+            "slug",
+            "outlines"
+        };
 
         /// <summary>
         /// Default page size for pagination
@@ -81,7 +91,7 @@ namespace VirtoCommerce.XPurchase.Services
             return await _productService.GetByIdsAsync(ids.ToArray(), ResponseGroups.ToString());
         }
 
-    
+
 
         /// <summary>
         /// Map all <see cref="CatalogProduct"/> to <see cref="CartProduct"/>
