9f2cf4636f18c1211d8e60d93f507c05bc96ece0
Artyom Dudarev <ad@virtoway.com>
2023-03-15 11:30:25 +0200

    PT-11098: Don't apply product promotions twice (#386)

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/LoadProductsQuery.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/LoadProductsQuery.cs
@@ -3,5 +3,6 @@ namespace VirtoCommerce.XDigitalCatalog.Queries
     public class LoadProductsQuery : CatalogQueryBase<LoadProductResponse>
     {
         public string[] ObjectIds { get; set; }
+        public bool EvaluatePromotions { get; set; } = true;
     }
 }
