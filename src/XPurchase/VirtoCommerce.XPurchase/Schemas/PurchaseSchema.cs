9644c3a6b912d4e818aff7cead58faee73255f2b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-04-19 08:43:58 +0200

    PT-11641: add cultureName to wishlist query (#401)

--- src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
@@ -1124,7 +1124,8 @@ namespace VirtoCommerce.XPurchase.Schemas
             {
                 Name = "wishlist",
                 Arguments = new QueryArguments(
-                        new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "listId", Description = "List Id" }),
+                        new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "listId", Description = "List Id" },
+                        new QueryArgument<StringGraphType> { Name = "cultureName", Description = "Culture name (\"en-Us\")" }),
                 Type = GraphTypeExtenstionHelper.GetActualType<WishlistType>(),
                 Resolver = new AsyncFieldResolver<object>(async context =>
                 {
