f2484147ff59f05ae757c1d73a8dac49272492da
basilkot <basilkot@gmail.com>
2023-04-26 12:44:47 +0200

    remove unused CultureName property

--- src/XCMS/VirtoCommerce.ExperienceApiModule.XCMS/Queries/GetPageQuery.cs
+++ src/XCMS/VirtoCommerce.ExperienceApiModule.XCMS/Queries/GetPageQuery.cs
@@ -5,7 +5,6 @@ namespace VirtoCommerce.ExperienceApiModule.XCMS.Queries
     public class GetPageQuery : IQuery<GetPageResponse>
     {
         public string StoreId { get; set; }
-        public string CultureName { get; set; }
         public string Keyword { get; set; }
     }
 }
