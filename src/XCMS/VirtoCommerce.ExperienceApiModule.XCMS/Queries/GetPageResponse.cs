beadd8439aeee46281d57ccb6479c403c61e8518
Basil Kotov <basilkot@gmail.com>
2023-04-27 14:10:03 +0200

    add possibility to search static pages (#404)

---
+++ src/XCMS/VirtoCommerce.ExperienceApiModule.XCMS/Queries/GetPageResponse.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace VirtoCommerce.ExperienceApiModule.XCMS.Queries
+{
+    public class GetPageResponse
+    {
+        public IEnumerable<PageItem> Pages { get; set; }
+    }
+}
