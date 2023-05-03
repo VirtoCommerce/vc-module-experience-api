fa6af546f970ead492b5871524a04c01ab524691
Oleg Zhuk <zhukoo@gmail.com>
2023-05-03 13:53:45 +0200

    feat: Refactor to bring flexibility for customization, ex: if you want to allow buying the product with Zero price, you should ovveride  ProductIsBuyableSpecification.CheckPricePolicy and CartAggregate.CheckPricePolicy.

--- src/XPurchase/VirtoCommerce.XPurchase/Specifications/ProductIsBuyableSpecification.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Specifications/ProductIsBuyableSpecification.cs
@@ -2,14 +2,36 @@ using System;
 
 namespace VirtoCommerce.XPurchase
 {
+    /// <summary>
+    /// Represents a product is buyable specification.
+    /// </summary>
     public class ProductIsBuyableSpecification
     {
+        /// <summary>
+        /// Evaluates a product is buyable specification.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public virtual bool IsSatisfiedBy(CartProduct product)
         {
             if (product == null)
                 throw new ArgumentNullException(nameof(product));
 
-            return product.Product.IsActive.GetValueOrDefault(false) && product.Product.IsBuyable.GetValueOrDefault(false) && product.Price != null;
+            return product.Product.IsActive.GetValueOrDefault(false) &&
+                product.Product.IsBuyable.GetValueOrDefault(false) &&
+                product.Price != null &&
+                CheckPricePolicy(product);
+        }
+
+        /// <summary>
+        /// Represents a price policy for a product. By default, product price should be greater than zero.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        protected virtual bool CheckPricePolicy(CartProduct product)
+        {
+            return product.Price.ActualPrice != 0;
         }
     }
 }
--- src/XPurchase/VirtoCommerce.XPurchase/Validators/CartLineItemPriceChangedValidator.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Validators/CartLineItemPriceChangedValidator.cs
@@ -18,12 +18,23 @@ namespace VirtoCommerce.XPurchase.Validators
                     cartProduct?.Price != null)
                 {
                     var tierPrice = cartProduct.Price.GetTierPrice(lineItem.Quantity);
-                    if (tierPrice.ActualPrice.Amount > 0 && tierPrice.ActualPrice.Amount != lineItem.SalePrice)
+                    if (HasPriceChanged(lineItem.SalePrice, tierPrice.ActualPrice.Amount))
                     {
                         context.AddFailure(CartErrorDescriber.ProductPriceChangedError(lineItem, lineItem.SalePrice, lineItem.SalePriceWithTax, tierPrice.ActualPrice.Amount, tierPrice.ActualPriceWithTax.Amount));
                     }
                 }
             });
+        }
+
+        /// <summary>
+        /// Responsible for determining if a product's price has changed from its current value to a new value. 
+        /// </summary>
+        /// <param name="currentPrice"></param>
+        /// <param name="newPrice"></param>
+        /// <returns>Returns true if price has changed, false if price has not changed.</returns>
+        protected virtual bool HasPriceChanged(decimal currentPrice, decimal newPrice)
+        {
+            return newPrice > 0 && currentPrice != newPrice;
         }
     }
 }
