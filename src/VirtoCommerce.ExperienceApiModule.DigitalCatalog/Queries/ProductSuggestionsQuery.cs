06b95b16ac2134256956513afa3a079cd359604e
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-05-25 15:53:28 +0200

    PT-11860: remove field from productSuggestions query (#416)

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/ProductSuggestionsQuery.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/ProductSuggestionsQuery.cs
@@ -7,21 +7,21 @@ namespace VirtoCommerce.XDigitalCatalog.Queries;
 
 public class ProductSuggestionsQuery : Query<ProductSuggestionsQueryResponse>
 {
+    public string StoreId { get; set; }
     public string Query { get; set; }
-    public IList<string> Fields { get; set; }
     public int Size { get; set; }
 
     public override IEnumerable<QueryArgument> GetArguments()
     {
+        yield return Argument<NonNullGraphType<StringGraphType>>(nameof(StoreId));
         yield return Argument<StringGraphType>(nameof(Query));
-        yield return Argument<ListGraphType<StringGraphType>>(nameof(Fields));
         yield return Argument<IntGraphType>(nameof(Size));
     }
 
     public override void Map(IResolveFieldContext context)
     {
+        StoreId = context.GetArgument<string>(nameof(StoreId));
         Query = context.GetArgument<string>(nameof(Query));
-        Fields = context.GetArgument<IList<string>>(nameof(Fields));
         Size = context.GetArgument<int>(nameof(Size));
     }
 }
