fbd7e9541571dcb74e15980f3be95f7c744b056a
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-07-11 14:10:05 +0200

    PT-1176: load child categories in category query (#426)

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/SearchCategoryQueryHandler.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Queries/SearchCategoryQueryHandler.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using MediatR;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
 using VirtoCommerce.ExperienceApiModule.Core.Pipelines;
 using VirtoCommerce.ExperienceApiModule.XDigitalCatalog.Index;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.GenericCrud;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Core.Services;
@@ -24,36 +27,30 @@ namespace VirtoCommerce.XDigitalCatalog.Queries
         private readonly ISearchPhraseParser _phraseParser;
         private readonly ICrudService<Store> _storeService;
         private readonly IGenericPipelineLauncher _pipeline;
+        private readonly IMediator _mediator;
 
         public SearchCategoryQueryHandler(
-            ISearchProvider searchProvider
-            , IMapper mapper
-            , ISearchPhraseParser phraseParser
-            , IStoreService storeService
-            , IGenericPipelineLauncher pipeline)
+            ISearchProvider searchProvider,
+            IMapper mapper,
+            ISearchPhraseParser phraseParser,
+            IStoreService storeService,
+            IGenericPipelineLauncher pipeline,
+            IMediator mediator)
         {
             _searchProvider = searchProvider;
             _mapper = mapper;
             _phraseParser = phraseParser;
             _storeService = (ICrudService<Store>)storeService;
             _pipeline = pipeline;
+            _mediator = mediator;
         }
 
         public virtual async Task<SearchCategoryResponse> Handle(SearchCategoryQuery request, CancellationToken cancellationToken)
         {
             var essentialTerms = new List<string>();
-            Store store = null;
+            var store = await GetStore(request);
 
-            if (!string.IsNullOrWhiteSpace(request.StoreId))
-            {
-                store = await _storeService.GetByIdAsync(request.StoreId);
-                if (store == null)
-                {
-                    throw new ArgumentException($"Store with Id: {request.StoreId} is absent");
-                }
-
-                essentialTerms.Add($"__outline:{store.Catalog}");
-            }
+            essentialTerms.Add($"__outline:{store.Catalog}");
 
             var searchRequest = new IndexSearchRequestBuilder()
                                           .WithFuzzy(request.Fuzzy, request.FuzzyLevel)
@@ -85,16 +82,84 @@ namespace VirtoCommerce.XDigitalCatalog.Queries
 
             await _pipeline.Execute(result);
 
+            if (request.GetLoadChildCategories())
+            {
+                var childCategoriesQuery = new ChildCategoriesQuery
+                {
+                    OnlyActive = true,
+                    Store = store,
+                    StoreId = request.StoreId,
+                    UserId = request.UserId,
+                    CultureName = request.CultureName,
+                    CurrencyCode = request.CurrencyCode,
+                };
+
+                var childCategoriesSearchQuery = new SearchCategoryQuery
+                {
+                    Store = store,
+                    StoreId = request.StoreId,
+                    UserId = request.UserId,
+                    CultureName = request.CultureName,
+                    CurrencyCode = request.CurrencyCode,
+                };
+
+                var regex = new Regex("^childCategories\\.");
+
+                foreach (var expCategory in result.Results)
+                {
+                    if (expCategory.ChildCategories != null)
+                    {
+                        continue;
+                    }
+
+                    childCategoriesQuery.CategoryId = expCategory.Id;
+                    childCategoriesQuery.MaxLevel = expCategory.Level;
+
+                    var response = await _mediator.Send(childCategoriesQuery, cancellationToken);
+                    var categoryIds = response.ChildCategories.Select(x => x.Key).ToArray();
+
+                    if (categoryIds.IsNullOrEmpty())
+                    {
+                        continue;
+                    }
+
+                    childCategoriesSearchQuery.ObjectIds = categoryIds;
+                    childCategoriesSearchQuery.Take = categoryIds.Length;
+                    childCategoriesSearchQuery.IncludeFields = request.IncludeFields.Where(x => x.StartsWith("childCategories.")).Select(x => regex.Replace(x, string.Empty, 1)).ToList();
+
+                    var childCategories = await Handle(childCategoriesSearchQuery, cancellationToken);
+
+                    expCategory.ChildCategories = childCategories.Results.ToList();
+                }
+            }
+
             return result;
         }
 
         public virtual async Task<LoadCategoryResponse> Handle(LoadCategoryQuery request, CancellationToken cancellationToken)
         {
             var searchRequest = _mapper.Map<SearchCategoryQuery>(request);
+            searchRequest.Store = await GetStore(request);
 
             var result = await Handle(searchRequest, cancellationToken);
 
             return new LoadCategoryResponse(result.Results);
+        }
+
+        private async Task<Store> GetStore<T>(CatalogQueryBase<T> request)
+        {
+            var store = request.Store;
+
+            if (store is null && !string.IsNullOrWhiteSpace(request.StoreId))
+            {
+                store = await _storeService.GetByIdAsync(request.StoreId);
+                if (store == null)
+                {
+                    throw new ArgumentException($"Store with Id: {request.StoreId} is absent");
+                }
+            }
+
+            return store;
         }
     }
 }
