24f6d47c26ead09a3955d25fbea14ba61c863911
Artyom Dudarev <ad@virtoway.com>
2023-09-11 16:27:52 +0300

    PT-13290: Use correct authorization error codes (#446)

--- src/VirtoCommerce.ExperienceApiModule.Core/Services/IUserManagerCore.cs
+++ src/VirtoCommerce.ExperienceApiModule.Core/Services/IUserManagerCore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Security;
 
@@ -6,6 +7,10 @@ namespace VirtoCommerce.ExperienceApiModule.Core.Services
     public interface IUserManagerCore
     {
         Task<bool> IsLockedOutAsync(ApplicationUser user);
+
+        [Obsolete("Use CheckUserState(string userId, bool allowAnonymous)", DiagnosticId = "VC0005", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         Task CheckUserState(string userId);
+
+        Task CheckUserState(string userId, bool allowAnonymous);
     }
 }
