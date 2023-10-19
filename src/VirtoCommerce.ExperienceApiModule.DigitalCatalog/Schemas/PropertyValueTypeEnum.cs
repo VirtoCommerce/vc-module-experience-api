deb2e0a315d81e48ddf3e705c6bb4f2ef9bbb766
Aleksandr Vishniakov <av@virtoway.com>
2023-10-19 03:20:07 +0200

    Change type name

--- src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/PropertyValueTypeEnum.cs
+++ src/VirtoCommerce.ExperienceApiModule.DigitalCatalog/Schemas/PropertyValueTypeEnum.cs
@@ -5,5 +5,10 @@ namespace VirtoCommerce.XDigitalCatalog.Schemas
 {
     public class PropertyValueTypeEnum: EnumerationGraphType<PropertyValueType>
     {
+        public PropertyValueTypeEnum()
+        {
+            Name = "PropertyValueTypes";
+            Description = "The type of catalog property value.";
+        }
     }
 }
