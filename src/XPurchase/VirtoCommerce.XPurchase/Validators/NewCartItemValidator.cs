0a3a6210a6df7503cac084e266339b50c7fb7868
Oleg Zhuk <zhukoo@gmail.com>
2023-11-28 11:35:46 +0200

    PT-14563: Extendable Cart Validation Policies (#490)
    feat: Refactor code to customize cart validation policies to allow adding a product with zero price.
    feat: Refactor code to customize cart validation policies to allow adding a product with a custom price.
    
    ### Allow Adding Products with Zero Price
    1. Create Virto Commerce extensions module for VirtoCommerce.ExperienceApi.
    2. Create a new class CustomCatalogProductIsBuyableSpecification inherited from CatalogProductIsBuyableSpecification.
    3. Override IsSatisfiedBy method in CustomCatalogProductIsBuyableSpecification to include additional conditions for buyability.
    4. Create a new class CustomProductIsBuyableSpecification inherited from ProductIsBuyableSpecification.
    5. Override IsSatisfiedBy method in CustomProductIsBuyableSpecification.
    6. Implemente CheckPricePolicy to allow buying products with zero price.
    7. Override specs in Module.Initialize for both ProductIsBuyableSpecification and CatalogProductIsBuyableSpecification.
    
    Sample Source:
    ```cs
        public class CustomCatalogProductIsBuyableSpecification : CatalogProductIsBuyableSpecification
        {
            public override bool IsSatisfiedBy(ExpProduct product)
            {
                if (product == null)
                    throw new ArgumentNullException(nameof(product));
    
                return product.IndexedProduct.IsActive.GetValueOrDefault(false)
                    && product.IndexedProduct.IsBuyable.GetValueOrDefault(false);
            }
        }
    
        public class CustomProductIsBuyableSpecification : ProductIsBuyableSpecification
        {
            public override bool IsSatisfiedBy(CartProduct product)
            {
                return base.IsSatisfiedBy(product);
            }
    
            protected override bool CheckPricePolicy(CartProduct product)
            {
                // Allow to buy product with zero price
                return true;
            }
        }
     ```
    
    Override specs in Module.Initialize:
    ```cs
        public void Initialize(IServiceCollection serviceCollection)
        {
            // Override models
            AbstractTypeFactory<ProductIsBuyableSpecification>.OverrideType<ProductIsBuyableSpecification, CustomProductIsBuyableSpecification>();
            AbstractTypeFactory<CatalogProductIsBuyableSpecification>.OverrideType<CatalogProductIsBuyableSpecification, CustomCatalogProductIsBuyableSpecification>();
        }
    ```
    
    ### Allow Adding Products with Custom Price
    1. Create a new class CustomNewCartItemValidator inherited from NewCartItemValidator.
    2. Override ValidateTierPrice virtual method in CustomNewCartItemValidator to customize tier price validation.
    3. Create a new class CustomCartAggregate inherited from CartAggregate.
    4. Override SetLineItemTierPrice in CustomCartAggregate to handle custom tier price logic.
    5. Override NewCartItemValidator in Module.Initialize to use CustomNewCartItemValidator.
    6. Override CartAggregate in Module.Initialize to use CustomCartAggregate .
    
    ```cs
        public void Initialize(IServiceCollection serviceCollection)
        {
            // Override models
          AbstractTypeFactory<NewCartItemValidator>.OverrideType<NewCartItemValidator, CustomNewCartItemValidator>();
          serviceCollection.AddTransient<CartAggregate, CustomCartAggregate>();
          services.AddTransient<Func<CartAggregate>>(provider => () => provider.CreateScope().ServiceProvider.GetRequiredService<CustomCartAggregate>());
        }
    ```

--- src/XPurchase/VirtoCommerce.XPurchase/Validators/NewCartItemValidator.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Validators/NewCartItemValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.XPurchase.Validators
 {
@@ -15,70 +16,110 @@ namespace VirtoCommerce.XPurchase.Validators
             {
                 RuleFor(x => x).Custom((newCartItem, context) =>
                 {
-                    if (newCartItem.CartProduct != null)
-                    {
-                        var cartProduct = newCartItem.CartProduct;
-
-                        // PRODUCT_FFC_QTY
-                        if (cartProduct.Product.TrackInventory.GetValueOrDefault(false))
-                        {
-                            var result = cartProduct.Inventory != null;
-                            if (result)
-                            {
-                                result = cartProduct.Inventory.AllowPreorder ||
-                                         cartProduct.Inventory.AllowBackorder ||
-                                         cartProduct.AvailableQuantity >= newCartItem.Quantity;
-                            }
-
-                            if (!result)
-                            {
-                                context.AddFailure(CartErrorDescriber.ProductAvailableQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, cartProduct.AvailableQuantity));
-                            }
-                        }
-
-                        // CART_PRODUCT_UNAVAILABLE
-                        if (!cartProduct.Product.IsBuyable.GetValueOrDefault(false))
-                        {
-                            context.AddFailure(CartErrorDescriber.ProductUnavailableError(nameof(CatalogProduct), cartProduct?.Product?.Id));
-                        }
-
-                        // CART_PRODUCT_INACTIVE
-                        if (!cartProduct.Product.IsActive.GetValueOrDefault(false))
-                        {
-                            context.AddFailure(CartErrorDescriber.ProductInactiveError(nameof(CatalogProduct), cartProduct?.Product?.Id));
-                        }
-
-                        // PRODUCT_PRICE_INVALID
-                        if (cartProduct.Price == null || cartProduct.Price.ListPrice == 0.0)
-                        {
-                            context.AddFailure(CartErrorDescriber.ProductNoPriceError(nameof(CatalogProduct), cartProduct?.Product?.Id));
-                        }
-
-                        // PRODUCT_MIN_QTY
-                        var minQuantity = cartProduct?.Product?.MinQuantity;
-                        if (newCartItem.Quantity < minQuantity)
-                        {
-                            context.AddFailure(CartErrorDescriber.ProductMinQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, minQuantity.Value));
-                        }
-
-                        // PRODUCT_MAX_QTY
-                        var maxQuantity = cartProduct?.Product?.MaxQuantity;
-                        if (maxQuantity > 0 && newCartItem.Quantity > maxQuantity)
-                        {
-                            context.AddFailure(CartErrorDescriber.ProductMaxQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, maxQuantity.Value));
-                        }
-                    }
-                    if (newCartItem.Price != null)
-                    {
-                        var productSalePrice = newCartItem.CartProduct.Price.GetTierPrice(newCartItem.Quantity).Price;
-
-                        if (newCartItem.Price < productSalePrice.Amount)
-                        {
-                            context.AddFailure(CartErrorDescriber.UnableToSetLessPrice(newCartItem.CartProduct));
-                        }
-                    }
+                    ValidateCartItem(context, newCartItem);
                 });
             });
+        }
+
+        /// <summary>
+        /// Performs custom validation for the NewCartItem.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="newCartItem"></param>
+        protected virtual void ValidateCartItem(ValidationContext<NewCartItem> context, NewCartItem newCartItem)
+        {
+            if (newCartItem.CartProduct != null)
+            {
+                var cartProduct = newCartItem.CartProduct;
+
+                ValidateProductIsBuyable(context, cartProduct);
+
+                ValidateProductInventory(context, cartProduct, newCartItem);
+
+                ValidateMinMaxQuantity(context, cartProduct, newCartItem);
+            }
+
+            if (newCartItem.Price != null)
+            {
+                ValidateTierPrice(context, newCartItem);
+            }
+        }
+
+        protected virtual void ValidateProductIsBuyable(ValidationContext<NewCartItem> context, CartProduct cartProduct)
+        {
+            if (!AbstractTypeFactory<ProductIsBuyableSpecification>.TryCreateInstance().IsSatisfiedBy(cartProduct))
+            {
+                // CART_PRODUCT_UNAVAILABLE
+                if (!cartProduct.Product.IsBuyable.GetValueOrDefault(false))
+                {
+                    context.AddFailure(CartErrorDescriber.ProductUnavailableError(nameof(CatalogProduct), cartProduct?.Product?.Id));
+                }
+
+                // CART_PRODUCT_INACTIVE
+                if (!cartProduct.Product.IsActive.GetValueOrDefault(false))
+                {
+                    context.AddFailure(CartErrorDescriber.ProductInactiveError(nameof(CatalogProduct), cartProduct?.Product?.Id));
+                }
+
+                // PRODUCT_PRICE_INVALID
+                if (cartProduct.Price == null || cartProduct.Price.ListPrice == 0.0)
+                {
+                    context.AddFailure(CartErrorDescriber.ProductNoPriceError(nameof(CatalogProduct), cartProduct?.Product?.Id));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validates the tier price of the new cart item.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="newCartItem"></param>
+        protected virtual void ValidateTierPrice(ValidationContext<NewCartItem> context, NewCartItem newCartItem)
+        {
+            var productSalePrice = newCartItem.CartProduct.Price.GetTierPrice(newCartItem.Quantity).Price;
+
+            if (newCartItem.Price < productSalePrice.Amount)
+            {
+                context.AddFailure(CartErrorDescriber.UnableToSetLessPrice(newCartItem.CartProduct));
+            }
+        }
+
+        /// <summary>
+        /// Validates the min and max quantity constraints for the new cart item.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="cartProduct"></param>
+        /// <param name="newCartItem"></param>
+        protected virtual void ValidateMinMaxQuantity(ValidationContext<NewCartItem> context, CartProduct cartProduct, NewCartItem newCartItem)
+        {
+            // PRODUCT_MIN_QTY
+            var minQuantity = cartProduct?.Product?.MinQuantity;
+            if (newCartItem.Quantity < minQuantity)
+            {
+                context.AddFailure(CartErrorDescriber.ProductMinQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, minQuantity.Value));
+            }
+
+            // PRODUCT_MAX_QTY
+            var maxQuantity = cartProduct?.Product?.MaxQuantity;
+            if (maxQuantity > 0 && newCartItem.Quantity > maxQuantity)
+            {
+                context.AddFailure(CartErrorDescriber.ProductMaxQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, maxQuantity.Value));
+            }
+        }
+
+
+        /// <summary>
+        /// Validates the product inventory of the new cart item
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="cartProduct"></param>
+        /// <param name="newCartItem"></param>
+        protected virtual void ValidateProductInventory(ValidationContext<NewCartItem> context, CartProduct cartProduct, NewCartItem newCartItem)
+        {
+            if (!AbstractTypeFactory<ProductIsAvailableSpecification>.TryCreateInstance().IsSatisfiedBy(cartProduct, newCartItem.Quantity))
+            {
+                context.AddFailure(CartErrorDescriber.ProductAvailableQuantityError(nameof(CatalogProduct), cartProduct?.Product?.Id, newCartItem.Quantity, cartProduct.AvailableQuantity));
+            }
         }
     }
 }
