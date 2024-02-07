60602e7dc342107937e667c4e5cdf54eaca903c5
Evgeny Polyakov <epo@virtoway.com>
2024-02-07 14:23:26 +0300

    Refactoring

--- src/XPurchase/VirtoCommerce.XPurchase/Commands/CloneWishlistCommandHandler.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Commands/CloneWishlistCommandHandler.cs
@@ -2,6 +2,8 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using VirtoCommerce.CartModule.Core.Model;
+using VirtoCommerce.CartModule.Core.Services;
 using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.Platform.Core.Common;
@@ -11,13 +13,16 @@ namespace VirtoCommerce.XPurchase.Commands;
 public class CloneWishlistCommandHandler : CartCommandHandler<CloneWishlistCommand>
 {
     private readonly IMemberResolver _memberResolver;
+    private readonly IShoppingCartService _shoppingCartService;
 
     public CloneWishlistCommandHandler(
         ICartAggregateRepository cartAggregateRepository,
-        IMemberResolver memberResolver)
+        IMemberResolver memberResolver,
+        IShoppingCartService shoppingCartService)
         : base(cartAggregateRepository)
     {
         _memberResolver = memberResolver;
+        _shoppingCartService = shoppingCartService;
     }
 
     public override async Task<CartAggregate> Handle(CloneWishlistCommand request, CancellationToken cancellationToken)
@@ -25,27 +30,25 @@ public class CloneWishlistCommandHandler : CartCommandHandler<CloneWishlistComma
         request.CartType = XPurchaseConstants.ListTypeName;
         var cloneCartAggregate = await CreateNewCartAggregateAsync(request);
 
-        cloneCartAggregate.Cart.Name = request.ListName;
+        var contact = await _memberResolver.ResolveMemberByIdAsync(request.UserId) as Contact;
+
         cloneCartAggregate.Cart.Description = request.Description;
+        cloneCartAggregate.Cart.CustomerName = contact?.Name;
 
         if (request.Scope?.EqualsInvariant(XPurchaseConstants.OrganizationScope) == true)
         {
-            var contact = await _memberResolver.ResolveMemberByIdAsync(request.UserId) as Contact;
-            
             var organizationId = contact?.Organizations?.FirstOrDefault();
 
             cloneCartAggregate.Cart.OrganizationId = organizationId;
         }
 
-        var cartAggregate = request.WishlistUserContext.Cart == null
-            ? await CartRepository.GetCartByIdAsync(request.ListId)
-            : await CartRepository.GetCartForShoppingCartAsync(request.WishlistUserContext.Cart);
+        var cart = request.WishlistUserContext.Cart ?? await _shoppingCartService.GetByIdAsync(request.ListId, nameof(CartResponseGroup.Default));
 
-        if (cartAggregate != null)
+        if (cart != null)
         {
             cloneCartAggregate.ValidationRuleSet = new[] { "default" };
 
-            var items = cartAggregate.Cart?.Items?
+            var items = cart.Items?
                             .Select(x => new NewCartItem(x.ProductId, x.Quantity) { IsWishlist = true })
                             .ToArray()
                         ?? Array.Empty<NewCartItem>();
