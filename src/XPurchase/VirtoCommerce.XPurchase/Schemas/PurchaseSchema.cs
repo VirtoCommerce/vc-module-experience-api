feaefec2dccea2f97dd1ac855760bab0cb16d1b9
Evgeny Polyakov <epo@virtoway.com>
2024-02-07 14:27:57 +0300

    Fix respGroup

--- src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/Schemas/PurchaseSchema.cs
@@ -1602,7 +1602,7 @@ namespace VirtoCommerce.XPurchase.Schemas
 
         private async Task CheckAuthAsyncByCartId(IResolveFieldContext context, string cartId)
         {
-            var cart = await _cartService.GetByIdAsync(cartId, CartResponseGroup.Default.ToString());
+            var cart = await _cartService.GetByIdAsync(cartId);
 
             if (cart == null)
             {
