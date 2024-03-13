2c2ff1731537a414696f5e934af8a5cf722e21bd
Oleg Zhuk <zhukoo@gmail.com>
2024-03-13 13:40:00 +0200

    VCST-596: Extend store settings with installed module id and public settings (#535)
    feat: Expanded store settings query to include installed modules and public store settings for each module. This enhancement enables frontend applications to efficiently access module settings. Set the store platform setting to true to ensure accessibility and visibility of settings across the store query in XAPI. Example of the modules with public settings: Google Analytics, Hotjar, etc.

---
+++ src/VirtoCommerce.ExperienceApiModule.Core/Queries/ModuleSettings.cs
@@ -0,0 +1,8 @@
+namespace VirtoCommerce.ExperienceApiModule.Core.Queries;
+
+public class ModuleSettings
+{
+    public string ModuleId { get; set; }
+
+    public ModuleSetting[] Settings { get; set; }
+}
