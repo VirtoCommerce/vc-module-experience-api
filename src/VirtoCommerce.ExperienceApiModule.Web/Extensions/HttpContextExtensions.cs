f5c6fdf80f07a5e9d4776d76bf46e9ddce67c2bd
Oleg Zhuk <zhukoo@gmail.com>
2024-04-16 21:44:08 +0200

    VCST-934: login-on-begalf for token auth
    feat: Adds the capability to perform login-on-behalf (impersonation) using token authentication within the XAPI framework. The new feature extends the /connect/token flow by adding support for a custom grant type called impersonate. Users can now leverage the grant_type=impersonate&user_id={user_id} parameter to set the vc_xapi_impersonated_customerid for impersonation purposes. Ensure that the account initiating the impersonation has the necessary platform:security:loginOnBehalf permission. To reset the impersonation , use the grant_type=impersonate and empty user_id.

--- src/VirtoCommerce.ExperienceApiModule.Web/Extensions/HttpContextExtensions.cs
+++ src/VirtoCommerce.ExperienceApiModule.Web/Extensions/HttpContextExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using OpenIddict.Abstractions;
+using VirtoCommerce.ExperienceApiModule.Core;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
 using VirtoCommerce.Platform.Core;
 using VirtoCommerce.Platform.Core.Security;
@@ -12,15 +13,62 @@ namespace VirtoCommerce.ExperienceApiModule.Web.Extensions
 {
     public static class HttpContextExtensions
     {
+        public const string ImpersonatedCustomerIdClaimType = "vc_xapi_impersonated_customerid";
+
         public static GraphQLUserContext BuildGraphQLUserContext(this HttpContext context)
         {
             var principal = context.User;
             var operatorUserName = default(string);
 
             // Impersonate a user based on their VC account object id by passing that value along with the header VirtoCommerce-User-Name.
-            if (principal != null
-                && context.Request.Headers.TryGetValue("VirtoCommerce-User-Name", out var userNameFromHeader)
-                && principal.IsInRole(PlatformConstants.Security.SystemRoles.Administrator))
+            if (principal != null)
+            {
+                if (!TryResolveLegacyLoginOnBehalf(context, ref principal, ref operatorUserName))
+                {
+                    TryResolveTokenLoginOnBehalf(context, ref principal, ref operatorUserName);
+                }
+            }
+
+            var userContext = new GraphQLUserContext(principal);
+
+            if (!string.IsNullOrEmpty(operatorUserName))
+            {
+                userContext.TryAdd("OperatorUserName", operatorUserName);
+            }
+
+            return userContext;
+        }
+
+        private static string GetCurrentUserId(ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal?.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? claimsPrincipal?.FindFirstValue("name") ?? AnonymousUser.UserName;
+        }
+
+        private static bool TryResolveTokenLoginOnBehalf(HttpContext context, ref ClaimsPrincipal principal, ref string operatorUserName)
+        {
+            var impersonatedCustomerId = principal.FindFirstValue(ImpersonatedCustomerIdClaimType);
+
+            if (!string.IsNullOrEmpty(impersonatedCustomerId))
+            {
+                var factory = context.RequestServices.GetService<Func<SignInManager<ApplicationUser>>>();
+                var signInManager = factory();
+                var user = signInManager.UserManager.FindByIdAsync(impersonatedCustomerId).GetAwaiter().GetResult();
+                if (user != null)
+                {
+                    operatorUserName = context.User.Identity.Name;
+                    principal = signInManager.CreateUserPrincipalAsync(user).GetAwaiter().GetResult();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryResolveLegacyLoginOnBehalf(HttpContext context, ref ClaimsPrincipal principal, ref string operatorUserName)
+        {
+            if (context.Request.Headers.TryGetValue("VirtoCommerce-User-Name", out var userNameFromHeader)
+                            && principal.IsInRole(PlatformConstants.Security.SystemRoles.Administrator))
             {
                 if (userNameFromHeader == "Anonymous")
                 {
@@ -49,16 +97,11 @@ namespace VirtoCommerce.ExperienceApiModule.Web.Extensions
                         operatorUserName = operatorUserNameHeader;
                     }
                 }
+
+                return true;
             }
 
-            var userContext = new GraphQLUserContext(principal);
-
-            if (!string.IsNullOrEmpty(operatorUserName))
-            {
-                userContext.TryAdd("OperatorUserName", operatorUserName);
-            }
-
-            return userContext;
+            return false;
         }
     }
 }
