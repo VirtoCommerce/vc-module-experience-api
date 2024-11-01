acced7c51f59dc5dbdf3da315233a73015ab6a85
Oleg Zhuk <zhukoo@gmail.com>
2024-11-01 11:59:32 +0200

    VCST-2173: Empty totals if shipment is requested (#572)
    fix: Empty totals if shipment is requested

--- src/XPurchase/VirtoCommerce.XPurchase/CartAggregateResponseGroup.cs
+++ src/XPurchase/VirtoCommerce.XPurchase/CartAggregateResponseGroup.cs
@@ -5,12 +5,12 @@ namespace VirtoCommerce.XPurchase
     [Flags]
     public enum CartAggregateResponseGroup
     {
-        None = 0,
+        Default = 0,
         WithPayments = 1,
         WithLineItems = 1 << 1,
         WithShipments = 1 << 2,
-        Validate = 1 << 3,
-        WithDynamicProperties = 1 << 4,
-        Full = WithPayments | WithLineItems | WithShipments | Validate | WithDynamicProperties
+        WithDynamicProperties = 1 << 3,
+        RecalculateTotals = 1 << 4,
+        Full = Default | WithPayments | WithLineItems | WithShipments | WithDynamicProperties | RecalculateTotals
     }
 }
